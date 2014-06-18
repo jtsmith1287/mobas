b328951a677951c8507242889c9b36a4997b863e
Justin Smith <jtsmith1287@gmail.com>
2014-06-17 21:42:32 -0700

    Added basic main menu screen

---
+++ Assets/Scripts/MainMenuGui.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuGui : MonoBehaviour {
+
+	[SerializeField]
+	Texture2D _mainMenuBG;
+	[SerializeField]
+	Texture2D _startGameButton;
+
+	void OnGUI(){
+		GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), _mainMenuBG);
+		
+		if (GUI.Button(new Rect(Screen.width/2 -_startGameButton.width/2,
+							Screen.height/2 - _startGameButton.height/2,
+							256, 64), _startGameButton)) {
+			Application.LoadLevel("main");
+		}
+	}
+}
