1c7418f39590590687c5ed5401f1e4adc6d4694a
Justin Smith <jtsmith1287@gmail.com>
2014-07-26 22:26:12 -0700

    Wrong branch, my bad... added basic crosshair

---
+++ Assets/InGameGUI.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InGameGUI : MonoBehaviour {
+
+	public Texture2D crosshair;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void OnGui(){
+		
+	}
+}
---
+++ Assets/src/Game/PlayerGui.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerGui : MonoBehaviour {
+
+	public Texture2D crosshair;
+	public float crosshairSize = 4.0f;
+	float crosshairPosAdjust;
+	
+
+	// Use this for initialization
+	void Start () {
+		crosshairPosAdjust = crosshairSize/2;
+	}
+	
+	// Update is called once per frame
+	void OnGUI () {
+		GUI.DrawTexture(new Rect(Screen.width/2-crosshair.width/crosshairSize,
+		 		                 Screen.height/2-crosshair.height/crosshairSize,
+		 		                 crosshair.width/crosshairPosAdjust, crosshair.height/crosshairPosAdjust),
+		 		        crosshair);
+		 		        
+	}
+}
